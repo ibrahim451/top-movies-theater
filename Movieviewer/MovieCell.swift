84ed9ec977e50584c170a85e4e0736a5807fd12f
Ibrahim Mustafa <ibrahim451@Ibrahims-Mac-mini.local>
2016-02-13 16:19:38 -0800

    cloning

---
+++ Movieviewer/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  Movieviewer
+//
+//  Created by Ibrahim Mustafa on 2/12/16.
+//  Copyright © 2016 Ibrahim Mustafa. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
